4d27ac4fe9ad65d9e0387f429b155640cdb1b238
Flabio Hinestroza Sanclemente <flabiohs@gmail.com>
2024-08-26 09:29:25 -0500

    add new fields in the entitie school "ZipCode,StateId y ProviderNumber"

--- safe_msvc_school/insfractruture/entities/SchoolEntity.go
+++ safe_msvc_school/insfractruture/entities/SchoolEntity.go
@@ -3,12 +3,15 @@ package entities
 import "time"
 
 type School struct {
-	Id        uint       `gorm:"primary_key:auto_increment"`
-	Name      string     `gorm:"type:varchar(150);not null"`
-	Email     string     `gorm:"type:varchar(150);unique_index;not null"`
-	Address   string     `gorm:"type:varchar(150)"`
-	Phone     string     `gorm:"type:varchar(150);"`
-	Active    bool       `gorm:"type:boolean"`
-	CreatedAt time.Time  `gorm:"<-:created_at"`
-	UpdatedAt *time.Time `gorm:"type:TIMESTAMP(6)"`
+	Id             uint       `gorm:"primary_key:auto_increment"`
+	Name           string     `gorm:"type:varchar(150);not null"`
+	ProviderNumber string     `gorm:"type:varchar(150);not null"`
+	Email          string     `gorm:"type:varchar(150);unique_index;not null"`
+	Address        string     `gorm:"type:varchar(150)"`
+	Phone          string     `gorm:"type:varchar(150);"`
+	ZipCode        string     `gorm:"type:varchar(50);"`
+	StateId        uint       `gorm:"type:integer"`
+	Active         bool       `gorm:"type:boolean"`
+	CreatedAt      time.Time  `gorm:"<-:created_at"`
+	UpdatedAt      *time.Time `gorm:"type:TIMESTAMP(6)"`
 }
